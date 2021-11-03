c071c78cf15bc7eca5030dafd4a334a897d065ef
unipine <unipine29@gmail.com>
2021-11-03 08:18:08 -0400

    update pdf finished.

--- src/components/WithdrawSuccess.js
+++ src/components/WithdrawSuccess.js
@@ -3,44 +3,76 @@ import Box from '@material-ui/core/Box';
 import Grid from '@material-ui/core/Grid';
 import Paper from '@material-ui/core/Paper';
 import Button from '@material-ui/core/Button';
-import { jsPDF } from 'jspdf';
 import { useHistory } from "react-router-dom";
 import { PDFDownloadLink } from '@react-pdf/renderer';
-import { Document, Page, Text, View, StyleSheet, Image } from '@react-pdf/renderer';
+import { Document, Page, Text, View, StyleSheet, Image, Font } from '@react-pdf/renderer';
 import svgLogo from "../images/sacred_pdf_logo.png";
+import Mont_Regular from "../fonts/Montserrat-Regular.ttf";
+import Mont_Bold from "../fonts/Montserrat-Bold.ttf";
+
+Font.register({
+  family: 'Montserrat',
+  format: 'truetype',
+  fonts: [
+    {
+      src: Mont_Regular
+    },
+    {
+      src: Mont_Bold,
+      fontWeight: 'bold',
+    }
+  ]
+});
+
+Font.registerHyphenationCallback(word => {
+  const middle = Math.floor(word.length / 2);
+  const parts = word.length === 1 ? [word] : [word.substr(0, middle), word.substr(middle)];
+  return parts;
+});
 
 const styles = StyleSheet.create({
   page: {
     flexDirection: 'column',
     backgroundColor: '#FFFFFF',
-    // fontFamily: 'Montserrat',
+    fontFamily: 'Montserrat',
     textAlign: 'center',
   },
   section1: {
     flexDirection: 'column',
     alignItems: 'center',
     textAlign: 'center',
-    paddingTop: '32px',
+    paddingTop: '20px',
   },
   title: {
-    paddingTop: '17px',
+    paddingTop: '14px',
     fontWeight: 'bold',
     fontSize: "21.3px",
   },
   claimView: {
-    paddingTop: '27px',
+    paddingTop: '14px',
     flexDirection: 'row',
+    flexShrink: 1,
     justifyContent: 'center',
+    alignItems: 'center',
+  },
+  claimHead: {
+    fontWeight: 'bold', 
+    fontSize: '13.31px', 
+    paddingRight: 8
+  },
+  claimContent: {
+    fontSize: '13.31px', 
+    textAlign: 'left'
   },
   depositView: {
-    paddingTop: '28px',
+    paddingTop: '14px',
     flexDirection: 'column',
     alignItems: 'flex-start',
     marginRight: '100px',
     marginLeft: '100px',
   },
   warningView: {
-    paddingTop: '40px',
+    paddingTop: '14px',
     flexDirection: 'column',
     alignItems: 'flex-start',
     textAlign: 'left',
@@ -48,7 +80,6 @@ const styles = StyleSheet.create({
     marginLeft: '94px',
   },
   depositVerify: {
-    paddingTop: '10px',
     flexDirection: 'row',
     justifyContent: 'space-between',
     fontSize: '24px',
@@ -56,89 +87,22 @@ const styles = StyleSheet.create({
   depositContent: {
     paddingTop: '14px',
     flexDirection: 'row',
+    flexShrink: 1,
     textAlign: 'left',
   },
   field: {
-    width: '25%', 
+    width: '25%',
     textAlign: 'right',
     fontSize: '12px',
     fontWeight: 'bold',
     paddingRight: '14px',
   },
   value: {
-    width: '75%', 
-    textAlign: 'left', 
-    overflowWrap: 'break-word',
-    hyphens: 'auto',
+    width: '75%',
+    textAlign: 'left',
     fontSize: '12px'
   }
 });
-
-const MyDocument = () => (
-  <Document>
-    <Page size="A4" style={styles.page}>
-      <View style={styles.section1}>
-        <Image src={svgLogo} style={{ width: 200, height: 55 }} />
-        <Text style={styles.title}>Compliance Report</Text>
-      </View>
-      <View style={styles.claimView}>
-        <Text style={{ fontWeight: 700, fontSize: '13.31px', paddingRight: 8 }}>Sacred Claim:</Text>
-        <Text style={{ fontSize: '13.31px' }}>sacred-c72141ikwPJWb28121W28Kdj7HjHioa8sHSDfsef432g342G</Text>
-      </View>
-      <View style={styles.depositView}>
-        <Text style={{ fontSize: '12.42px' }}>Deposit</Text>
-        <View style={styles.depositVerify}>
-          <View style={{width: '50%', textAlign: 'left', overflowWrap: "anywhere"}}><Text>Verified</Text></View>
-          <View style={{width: '50%', textAlign: 'right', overflowWrap: "anywhere"}}><Text>10 CFX</Text></View>
-        </View>
-        <View style={styles.depositContent}>
-          <Text style={styles.field}>Date</Text>
-          <Text style={styles.value}>April 21, 2021 4:55 PM EST</Text>
-        </View>
-        <View style={styles.depositContent}>
-          <Text style={styles.field}>Transaction</Text>
-          <Text style={styles.value}>0xce1424fe02587fa23ff1e7702sdfsdfcb83a360d97e3c1722122742348980267c2512c31</Text>
-        </View>
-        <View style={styles.depositContent}>
-          <Text style={styles.field}>From</Text>
-          <Text style={styles.value}>0x123fs3dfgasy8121W2kAZod8Kdjioa</Text>
-        </View>
-        <View style={styles.depositContent}>
-          <Text style={styles.field}>Commitment</Text>
-          <Text style={styles.value}>0x6a3fe8cadfaf2a30384e0e715ff66d11a4e4e937dc84948c904b76502823edff</Text>
-        </View>
-      </View>
-      <View style={styles.depositView}>
-        <Text style={{ fontSize: '12.42px' }}>Withdrawal</Text>
-        <View style={styles.depositVerify}>
-          <View style={{width: '50%', textAlign: 'left', overflowWrap: "anywhere"}}><Text>Verified</Text></View>
-          <View style={{width: '50%', textAlign: 'right', overflowWrap: "anywhere"}}><Text>-0.02494 CFX</Text></View>
-        </View>
-        <View style={styles.depositContent}>
-          <Text style={styles.field}>Date</Text>
-          <Text style={styles.value}>June 30, 2021 2:27 AM EST</Text>
-        </View>
-        <View style={styles.depositContent}>
-          <Text style={styles.field}>Transaction</Text>
-          <Text style={styles.value}>0xad3351whs131we30384e0e715ff66d11a4e4e937dc8yuaeaw48c904b76502823edff</Text>
-        </View>
-        <View style={styles.depositContent}>
-          <Text style={styles.field}>From</Text>
-          <Text style={styles.value}>0x141ikwPqweqJWb28121W2kAZod8Kdjioa</Text>
-        </View>
-        <View style={styles.depositContent}>
-          <Text style={styles.field}>Commitment</Text>
-          <Text style={styles.value}>0xce1424fe02587fa23ff1e770283a360d97e3c1722122742348980267c2512c31</Text>
-        </View>
-      </View>
-      <View style={styles.warningView}>
-        <Text style={{ fontSize: '14.2px' }}>Warning</Text>
-        <Text style={{ color: '#686868', fontSize: '10.6px', paddingTop: '8px' }}>This Compliance Report is for informational purposes only. You should confirm the validity of this report by using Sacred’s Compliance Tool (https://app.sacred.finance/compliance) or with any other cryptographic software that can compute and verify the information contained herein(the "Sacred Inspect Tool"). Any discrepancies between information found in this report and provided by the above tool indicate that the information in this report is inaccurate and/or fraudulent.</Text>
-        <Text style={{ color: '#686868', fontSize: '10.6px', paddingTop: '8px' }}> THE COMPLIANCE REPORT IS PROVIDED "AS IS," WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OF THE SACRED COMPLIANCE TOOL BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THIS COMPLIANCE REPORT.</Text>
-      </View>
-    </Page>
-  </Document>
-);
 
 const WithdrawSuccess = ({ parsedNote, txReceipt, claim, deployment }) => {
   const history = useHistory();
@@ -148,6 +112,74 @@ const WithdrawSuccess = ({ parsedNote, txReceipt, claim, deployment }) => {
   }
 
   //TODO: add styling somehow?
+
+  const claimText = claim.substr(0, Math.floor(claim.length / 3)) + ' ' + claim.substr( Math.floor(claim.length / 3),  Math.floor(claim.length / 3) * 2) ;
+
+  const MyDocument = () => (
+    <Document>
+      <Page size="A4" style={styles.page}>
+        <View style={styles.section1}>
+          <Image src={svgLogo} style={{ width: 200, height: 55 }} />
+          <Text style={styles.title}>Compliance Report</Text>
+        </View>
+        <View style={styles.claimView}>
+          <Text style={styles.claimHead}>Sacred Claim:</Text>
+          <Text style={styles.claimContent}>{claimText}</Text>
+        </View>
+        <View style={styles.depositView}>
+          <Text style={{ fontSize: '12.42px' }}>Deposit</Text>
+          <View style={styles.depositVerify}>
+            <View style={{width: '50%', textAlign: 'left'}}><Text style={{fontWeight: 'bold'}}>Verified</Text></View>
+            <View style={{ width: '50%', textAlign: 'right'}}><Text style={{fontWeight: 'bold'}}>{`${parsedNote.amount} ${parsedNote.currency.toUpperCase()}`}</Text></View>
+          </View>
+          <View style={styles.depositContent}>
+            <Text style={styles.field}>Date</Text>
+            <Text style={styles.value}>{new Date(txReceipt.timestamp * 1000).toUTCString()}</Text>
+          </View>
+          <View style={styles.depositContent}>
+            <Text style={styles.field}>Transaction</Text>
+            <Text style={styles.value}>{txReceipt.transactionHash}</Text>
+          </View>
+          <View style={styles.depositContent}>
+            <Text style={styles.field}>From</Text>
+            <Text style={styles.value}>{txReceipt.from.toLowerCase()}</Text>
+          </View>
+          <View style={styles.depositContent}>
+            <Text style={styles.field}>Commitment</Text>
+            <Text style={styles.value}>{parsedNote.deposit.commitmentHex}</Text>
+          </View>
+        </View>
+        <View style={styles.depositView}>
+          <Text style={{ fontSize: '12.42px' }}>Withdrawal</Text>
+          <View style={styles.depositVerify}>
+            <View style={{width: '50%', textAlign: 'left'}}><Text style={{fontWeight: 'bold'}}>Verified</Text></View>
+            <View style={{ width: '50%', textAlign: 'right'}}><Text style={{fontWeight: 'bold'}}>- {txReceipt?.events?.Withdrawal?.returnValues?.fee} {parsedNote.currency.toUpperCase()}</Text></View>
+          </View>
+          <View style={styles.depositContent}>
+            <Text style={styles.field}>Date</Text>
+            <Text style={styles.value}>{new Date(txReceipt.timestamp * 1000).toUTCString()}</Text>
+          </View>
+          <View style={styles.depositContent}>
+            <Text style={styles.field}>Transaction</Text>
+            <Text style={styles.value}>{txReceipt.transactionHash}</Text>
+          </View>
+          <View style={styles.depositContent}>
+            <Text style={styles.field}>To</Text>
+            <Text style={styles.value}>{txReceipt.to.toLowerCase()}</Text>
+          </View>
+          <View style={styles.depositContent}>
+            <Text style={styles.field}>Nullifier Hash</Text>
+            <Text style={styles.value}>{txReceipt?.events?.Withdrawal?.returnValues?.nullifierHash}</Text>
+          </View>
+        </View>
+        <View style={styles.warningView}>
+          <Text style={{ fontSize: '14.2px', fontWeight: 'bold' }}>Warning</Text>
+          <Text style={{ color: '#686868', fontSize: '10.6px', paddingTop: '8px' }}>This Compliance Report is for informational purposes only. You should confirm the validity of this report by using Sacred’s Compliance Tool (https://app.sacred.finance/compliance) or with any other cryptographic software that can compute and verify the information contained herein(the "Sacred Inspect Tool"). Any discrepancies between information found in this report and provided by the above tool indicate that the information in this report is inaccurate and/or fraudulent.</Text>
+          <Text style={{ color: '#686868', fontSize: '10.6px', paddingTop: '8px' }}> THE COMPLIANCE REPORT IS PROVIDED "AS IS," WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OF THE SACRED COMPLIANCE TOOL BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THIS COMPLIANCE REPORT.</Text>
+        </View>
+      </Page>
+    </Document>
+  );
 
   return (
     <div>
@@ -177,7 +209,7 @@ const WithdrawSuccess = ({ parsedNote, txReceipt, claim, deployment }) => {
 
             <Grid item>
               <br />
-              <PDFDownloadLink document={<MyDocument />} fileName={"sacred_compliance_report"}>
+              <PDFDownloadLink document={<MyDocument />} fileName={"sacred_compliance_report"} style={{textDecoration: 'none'}}>
                 <Button variant="contained" color="secondary" style={{ textTransform: 'none', fontWeight: 'bold' }} fullWidth >Generate PDF</Button>
               </PDFDownloadLink>
             </Grid>
