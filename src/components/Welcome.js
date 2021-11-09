12510cfd2041c5c86232dd6b208f31904037f804
paulstaker <paulstaker@pm.me>
2021-11-09 00:40:12 -0500

    disable mainnet

--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -18,7 +18,7 @@ import { useTranslation } from "react-i18next";
 
 const injectedConnector = new InjectedConnector({
   supportedChainIds: [
-    1, // Ethereum Mainnet
+    // 1, // Ethereum Mainnet
     42, // Kovan Testnet
   ],
 });
