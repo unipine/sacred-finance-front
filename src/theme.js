0adbe2a71d0b3ecf9915bcd0ad70cf1ee31fae98
unipine <unipine29@gmail.com>
2021-11-08 20:54:36 -0600

    comment the old theme

--- src/theme.js
+++ src/theme.js
@@ -1,6 +1,59 @@
 import { createTheme, adaptV4Theme } from "@mui/material/styles";
 
-const Theme = createTheme(adaptV4Theme({
+// const Theme = createTheme(
+//   adaptV4Theme({
+//     palette: {
+//       primary: {
+//         main: "#EF646D",
+//       },
+//       secondary: {
+//         main: "#EF646D",
+//       },
+//       third: {
+//         main: "#5B5A99",
+//       },
+//     },
+//     overrides: {
+//       MuiPaper: {
+//         rounded: {
+//           borderRadius: "35px",
+//           minWidth: "300px",
+//         },
+//       },
+//       MuiButton: {
+//         root: {
+//           borderRadius: "35px",
+//         },
+//       },
+//       MuiInputBase: {
+//         root: {
+//           borderRadius: "35px",
+//         },
+//         input: {
+//           // marginLeft: "10px",
+//           // marginBottom: "10px",
+//         },
+//       },
+//       MuiFilledInput: {
+//         root: {
+//           borderRadius: "35px",
+//           borderTopLeftRadius: "35px",
+//           borderTopRightRadius: "35px",
+//         },
+//         underline: {
+//           color: "#fffff",
+//         },
+//       },
+//       MuiOutlinedInput: {
+//         root: {
+//           borderRadius: "25px",
+//         },
+//       },
+//     },
+//   })
+// );
+
+const Theme1 = createTheme({
   palette: {
     primary: {
       main: "#EF646D",
@@ -9,46 +62,56 @@ const Theme = createTheme(adaptV4Theme({
       main: "#EF646D",
     },
     third: {
-      main: '#5B5A99',
-    }
+      main: "#5B5A99",
+    },
   },
   overrides: {
     MuiPaper: {
-      rounded: {
-        borderRadius: "35px",
-        minWidth: "300px",
+      styleOverrides: {
+        rounded: {
+          borderRadius: "35px",
+          minWidth: "300px",
+        },
       },
     },
     MuiButton: {
-      root: {
-        borderRadius: "35px",
+      styleOverrides: {
+        root: {
+          borderRadius: "35px",
+        },
       },
     },
     MuiInputBase: {
-      root: {
-        borderRadius: "35px",
-      },
-      input: {
-        // marginLeft: "10px",
-        // marginBottom: "10px",
+      styleOverrides: {
+        root: {
+          borderRadius: "35px",
+        },
+        input: {
+          // marginLeft: "10px",
+          // marginBottom: "10px",
+        },
       },
     },
     MuiFilledInput: {
-      root: {
-        borderRadius: "35px",
-        borderTopLeftRadius: "35px",
-        borderTopRightRadius: "35px",
-      },
-      underline: {
-        color: "#fffff",
+      styleOverrides: {
+        root: {
+          borderRadius: "35px",
+          borderTopLeftRadius: "35px",
+          borderTopRightRadius: "35px",
+        },
+        underline: {
+          color: "#fffff",
+        },
       },
     },
     MuiOutlinedInput: {
-      root: {
-        borderRadius: "25px",
+      styleOverrides: {
+        root: {
+          borderRadius: "25px",
+        },
       },
     },
   },
-}));
+});
 
-export default Theme;
+export default Theme1;
