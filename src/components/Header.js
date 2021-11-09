febf6069a50063ae6e49aa6cbb78b64c5de39a9c
unipine <unipine29@gmail.com>
2021-11-09 14:15:13 -0600

    add the custom theme for header component

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,28 +4,63 @@ import Grid from "@mui/material/Grid";
 import Connect from "./Connect";
 import Version from "./Version";
 import LanguageSelect from "./LanguageSelect";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: "#bdbdbd",
+    },
+  },
+  components: {
+    MuiPaper: {
+      styleOverrides: {
+        rounded: {
+          borderRadius: "35px",
+          minWidth: "300px",
+        },
+      },
+    },
+    MuiButton: {
+      styleOverrides: {
+        root: {
+          borderRadius: "35px",
+        },
+      },
+    },
+    MuiOutlinedInput: {
+      styleOverrides: {
+        root: {
+          borderRadius: "25px",
+        },
+      },
+    },
+  },
+});
 
 const Header = ({ handleAlert, handleNetworkId, networkId }) => {
   return (
-    <div className="header">
-      <Grid
-        container
-        spacing={0}
-        direction="row"
-        justifyContent="space-between"
-        alignItems="center"
-      >
-        <Logo />
-        <Version
-          handleNetworkId={handleNetworkId}
-          networkId={networkId}
-          handleAlert={handleAlert}
-        />
-        <Connect handleAlert={handleAlert} networkId={networkId}/>
-        <LanguageSelect />
-        <Nav />
-      </Grid>
-    </div>
+    <ThemeProvider theme={theme}>
+      <div className="header">
+        <Grid
+          container
+          spacing={0}
+          direction="row"
+          justifyContent="space-between"
+          alignItems="center"
+        >
+          <Logo />
+          <Version
+            handleNetworkId={handleNetworkId}
+            networkId={networkId}
+            handleAlert={handleAlert}
+          />
+          <Connect handleAlert={handleAlert} networkId={networkId} />
+          <LanguageSelect />
+          <Nav />
+        </Grid>
+      </div>
+    </ThemeProvider>
   );
 };
 
