14fd9870a4810e8bba44d0941aa440d81b698cd6
unipine <unipine29@gmail.com>
2021-11-09 14:12:43 -0600

    set the popup menu position into (top, center)

--- src/components/LanguageSelect.js
+++ src/components/LanguageSelect.js
@@ -11,52 +11,70 @@ const CustomSelect = styled(Select)`
   &.Mui-focused {
     background-color: #ef646d;
     color: #ffffff;
+
+    fieldset {
+      border-color: #ef646d !important;
+    }
   }
   transition: all 0.5s;
 `;
 
 const CustomMenuItem = styled(MenuItem)`
-  &.MuiMenuItem-root Mui-selected,
-  &.Mui-selected, &.Mui-selected:hover {
+  &.Mui-selected,
+  &.Mui-selected:hover {
     background-color: rgba(0, 0, 0, 0.08);
   }
 `;
 
 const languageMap = {
-    en: {
-        label: "English",
-        active: true
-    },
-    ch: {
-        label: "中文",
-        active: false
-    }
+  en: {
+    label: "English",
+    active: true,
+  },
+  ch: {
+    label: "中文",
+    active: false,
+  },
 };
 
 const LanguageSelect = () => {
+  const [language, setLanguage] = useState("en");
 
-    const [language, setLanguage] = useState("en");
+  const handleChange = (e) => {
+    setLanguage(e.target.value);
+  };
 
-    const handleChange = (e) => {
-        setLanguage(e.target.value);
-    }
+  useEffect(() => {
+    i18next.changeLanguage(language);
+  }, [language]);
 
-    useEffect(() => {
-        i18next.changeLanguage(language);
-    }, [language]);
-
-    return (
-        <FormControl variant="outlined">
-            <CustomSelect
-                labelId="network-select-label"
-                value={language}
-                onChange={handleChange}
-            >
-                <CustomMenuItem button value="en">{languageMap.en.label}</CustomMenuItem>
-                <CustomMenuItem button value="ch">{languageMap.ch.label}</CustomMenuItem>
-            </CustomSelect>
-        </FormControl>
-    );
+  return (
+    <FormControl variant="outlined">
+      <CustomSelect
+        labelId="network-select-label"
+        value={language}
+        onChange={handleChange}
+        MenuProps={{
+          anchorOrigin: {
+            vertical: "top",
+            horizontal: "center",
+          },
+          transformOrigin: {
+            vertical: "top",
+            horizontal: "center",
+          },
+          getContentAnchorEl: null,
+        }}
+      >
+        <CustomMenuItem button value="en">
+          {languageMap.en.label}
+        </CustomMenuItem>
+        <CustomMenuItem button value="ch">
+          {languageMap.ch.label}
+        </CustomMenuItem>
+      </CustomSelect>
+    </FormControl>
+  );
 };
 
 export default LanguageSelect;
