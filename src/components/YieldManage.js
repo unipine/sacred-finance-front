46ce1c1ed835e35264eea8369ee40408246dc092
unipine <unipine29@gmail.com>
2021-11-11 17:44:43 -0600

    fix the UI of YieldManage component

--- src/components/YieldManage.js
+++ src/components/YieldManage.js
@@ -3,7 +3,6 @@ import Box from "@mui/material/Box";
 import Grid from "@mui/material/Grid";
 import Paper from "@mui/material/Paper";
 import Button from "@mui/material/Button";
-import makeStyles from '@mui/styles/makeStyles';
 import { useHistory } from "react-router-dom";
 import { useState, useEffect } from "react";
 
@@ -12,232 +11,259 @@ import { useLocation } from "react-router";
 import { InputLabel } from "@mui/material";
 import arrow_down from "../images/arrow_down.svg";
 import settingImg from "../images/setting.svg";
+import { styled } from "@mui/material/styles";
 
-const useStyles = makeStyles((theme) => ({
-    body: {
-        textAlign: "left",
-        fontFamily: [
-            'Montserrat'
-        ],
-    },
-    formControl: {
-    },
-    selectEmpty: {
-        marginTop: theme.spacing(2),
-    },
+const BalanceGrid = styled(Grid)`
+  border: 1px solid #757575;
+  border-radius: 1.25rem;
+  padding: 1rem;
+  margin-top: 20px;
+`;
 
-    headerBtn: {
-        marginTop: "-10px",
-        fontSize: "1.25rem",
-        fontFamily: "Montserrat",
-        textTransform: "none",
-        fontWeight: "bold",
-    },
-    textField: {
-        marginTop: theme.spacing(1),
-        "& .MuiInputBase-input": {
-            marginLeft: "10px",
-            marginBottom: "10px",
-        },
-    },
-    balance: {
-        border: "1px solid #757575",
-        borderRadius: '1.25rem',
-        padding: '1rem',
-    },
-    max: {
-        padding: '0.25rem',
-        marginRight: '0.5rem',
-        border: "1px solid #747474",
-        backgroundColor: "#8C8C8C",
-        borderRadius: "6px",
-        color: 'white',
-        fontSize: '0.85rem',
-    },
-    flex: {
-        display: 'flex',
-        alignItems: 'center',
-    },
-    button: {
-        marginTop: '80px',
-        fontSize: '1.25rem',
-    },
-    textBlack: {
-        color: 'black',
-        fontWeight: 700,
-    },
-    setting: {
-        marginTop: '1.25rem',
-        border: '4px solid #8CBDE9',
-        borderRadius: '0.5rem',
-        backgroundColor: 'rgba(140, 189, 233, 0.3)',
-        padding: '0.5rem',
-        width: '1.5rem'
-    }
-}));
+const CalculateButton = styled(Button)`
+  margin-top: 80px;
+  font-size: 1.25rem;
+`;
+
+const HeaderButton = styled(Button)`
+  margin-top: -10px;
+  font-size: 1.25rem;
+  font-family: Montserrat;
+  text-transform: none;
+  font-weight: bold;
+`;
+
+const CustomTextField = styled(TextField)`
+  margin: ${(props) => props.theme.spacing(1)};
+  padding-top: ${(props) => props.theme.spacing(1)};
+  padding-bottom: ${(props) => props.theme.spacing(1)};
+  border-radius: 22px;
+  background-color: rgba(0, 0, 0, 0.09);
+  $:hover {
+    background-color: rgba(0, 0, 0, 0.23);
+  }
+  &.MuiInputBase-input {
+    padding-top: 0;
+    margin-left: 10px;
+    margin-bottom: 10px;
+  }
+
+  & fieldset {
+    border: none;
+  }
+`;
+
+const BlackInputLabel = styled(InputLabel)`
+  color: black;
+  font-weight: 700;
+`;
+
+const FlexDiv = styled("div")`
+  display: flex;
+  align-items: center;
+
+  & div {
+    padding: 0.25rem;
+    margin-right: 0.5rem;
+    border: 1px solid #747474;
+    background-color: #8c8c8c;
+    border-radius: 6px;
+    color: white;
+    font-size: 0.85rem;
+  }
+`;
+
+const SettingDiv = styled("div")`
+  margin-top: 1.25rem;
+  border: 4px solid #8cbde9;
+  borderradius: 0.5rem;
+  backgroundcolor: rgba(140, 189, 233, 0.3);
+  padding: 0.5rem;
+  width: 1.5rem;
+`;
 
 const YieldManage = () => {
-    const history = useHistory();
-    const classes = useStyles();
-    const location = useLocation();
+  const history = useHistory();
+  const location = useLocation();
 
-    const [selectedWithdraw, setSelectedWithdraw] = useState(false);
-    const [coinType, setCoinType] = useState(0);
+  const [selectedWithdraw, setSelectedWithdraw] = useState(false);
+  const [coinType, setCoinType] = useState(0);
 
-    const handleCoinType = (e) => {
-        setCoinType(e.target.value);
-    }
+  const handleCoinType = (e) => {
+    setCoinType(e.target.value);
+  };
 
-    const handleWithdrawClick = () => {
-        history.push("/yieldWithdraw");
-        setSelectedWithdraw(true);
-    };
+  const handleWithdrawClick = () => {
+    history.push("/yieldWithdraw");
+    setSelectedWithdraw(true);
+  };
 
-    const handleRedeemClick = () => {
-        history.push("/yieldRedeem");
-        setSelectedWithdraw(false);
-    }
+  const handleRedeemClick = () => {
+    history.push("/yieldRedeem");
+    setSelectedWithdraw(false);
+  };
 
-    const handleCalculatePoints = () => {
-        history.push("/yieldRedeemConfirm");
-    }
+  const handleCalculatePoints = () => {
+    history.push("/yieldRedeemConfirm");
+  };
 
-    const handleWithrawYield = () => {
-        history.push("/yieldWithdrawConfirm");
-    }
+  const handleWithrawYield = () => {
+    history.push("/yieldWithdrawConfirm");
+  };
 
-    const handleClickRelayerSetting = () => {
-        history.push("/relayerSettings");
-    }
+  const handleClickRelayerSetting = () => {
+    history.push("/relayerSettings");
+  };
 
-    return (
-        <div>
-            <Paper>
-                <Box p={3} className={classes.body}>
-                    <Grid container direction="column" spacing={2}>
-                        <Grid
-                            item
-                            container
-                            direction="row"
-                            spacing={0}
-                            justifyContent="space-around"
-                            alignItems="center"
-                        >
-                            <Grid item>
-                                <Button
-                                    variant="text"
-                                    className={classes.headerBtn}
-                                    style={!selectedWithdraw ? { marginLeft: "10px" } : { color: "#A7A9AC", marginRight: "10px" }}
-                                    onClick={handleRedeemClick}
-                                >
-                                    <b>Redeem</b>
-                                </Button>
-                            </Grid>
-                            <Grid item>
-                                <Button
-                                    variant="text"
-                                    className={classes.headerBtn}
-                                    style={selectedWithdraw ? { marginLeft: "10px" } : { color: "#A7A9AC", marginRight: "10px" }}
-                                    onClick={handleWithdrawClick}
-                                >
-                                    <b>Withdraw</b>
-                                </Button>
-                            </Grid>
-                        </Grid>
+  return (
+    <div>
+      <Paper>
+        <Box p={3} sx={{ textAlign: "left", fontFamily: ["Montserrat"] }}>
+          <Grid container direction="column" spacing={2}>
+            <Grid
+              item
+              container
+              direction="row"
+              spacing={0}
+              justifyContent="space-around"
+              alignItems="center"
+            >
+              <Grid item>
+                <HeaderButton
+                  variant="text darkBlack"
+                  sx={
+                    !selectedWithdraw
+                      ? { marginLeft: "10px" }
+                      : { color: "#A7A9AC", marginRight: "10px" }
+                  }
+                  onClick={handleRedeemClick}
+                >
+                  <b>Redeem</b>
+                </HeaderButton>
+              </Grid>
+              <Grid item>
+                <HeaderButton
+                  sx={
+                    selectedWithdraw
+                      ? { marginLeft: "10px" }
+                      : { color: "#A7A9AC", marginRight: "10px" }
+                  }
+                  onClick={handleWithdrawClick}
+                >
+                  <b>Withdraw</b>
+                </HeaderButton>
+              </Grid>
+            </Grid>
 
-                        {
-                            selectedWithdraw ? (
-                                <>
-                                    <Grid item direction="column">
-                                        <Grid item className={classes.balance} style={{ marginTop: '20px' }}>
-                                            <Grid item container direction="row" justifyContent="space-between">
-                                                <InputLabel className={classes.textBlack}>From</InputLabel>
-                                                <InputLabel className={classes.textBlack}>Balance: 56078</InputLabel>
-                                            </Grid>
-                                            <Grid item container direction="row" justifyContent="space-between" alignItems="center" style={{ paddingTop: '10px' }}>
-                                                <InputLabel style={{ fontSize: '20px' }}>0.0</InputLabel>
-                                                <div className={classes.flex}>
-                                                    <div className={classes.max}>MAX</div>
-                                                    <select
-                                                        labelId="network-select-label"
-                                                        value={coinType}
-                                                        onChange={handleCoinType}
-                                                        className="select"
-                                                    >
-                                                        <option value={0}>USDT YP</option>
-                                                        <option value={1}>BNB</option>
-                                                    </select>
-                                                </div>
-                                            </Grid>
-                                        </Grid>
-                                        <Grid item style={{ textAlign: 'center', padding: '20px' }}>
-                                            <img src={arrow_down} />
-                                        </Grid>
-                                        <Grid item className={classes.balance}>
-                                            <Grid item container direction="row" justifyContent="space-between" style={{ paddingBottom: '10px' }}>
-                                                <InputLabel className={classes.textBlack}>To</InputLabel>
-                                                <InputLabel className={classes.textBlack}>Balance: 0.000000</InputLabel>
-                                            </Grid>
-                                            <Grid item container direction="row" justifyContent="space-between">
-                                                <InputLabel style={{ fontSize: '20px' }}>0.0</InputLabel>
-                                                <InputLabel style={{ fontSize: '20px' }}>USDT</InputLabel>
-                                            </Grid>
-                                        </Grid>
-                                        <Grid item container>
-                                            <Button
-                                                variant="contained"
-                                                color="secondary"
-                                                className={classes.button}
-                                                fullWidth
-                                                onClick={handleWithrawYield}
-                                            >
-                                                Withdraw Yield
-                                            </Button>
-                                        </Grid>
-                                    </Grid>
-                                </>
-                            ) : (
-                                <>
-                                    <Grid item direction="column" justifyContent="space-between">
-                                        <Grid item>
-                                            <InputLabel>Sacred Claim</InputLabel>
-                                            <TextField
-                                                className={classes.textField}
-                                                multiline
-                                                variant="filled"
-                                                size="small"
-                                                InputProps={{ disableUnderline: true }}
-                                                fullWidth
-                                            />
-                                        </Grid>
-                                        <Grid item>
-                                            <Button
-                                                variant="contained"
-                                                color="secondary"
-                                                className={classes.button}
-                                                fullWidth
-                                                style={{ marginTop: '280px', }}
-                                                onClick={handleCalculatePoints}
-                                            >
-                                                Calculate Points
-                                            </Button>
-                                        </Grid>
-                                    </Grid>
-                                </>
-                            )
-                        }
+            {selectedWithdraw ? (
+              <>
+                <Grid item direction="column">
+                  <BalanceGrid item>
+                    <Grid
+                      item
+                      container
+                      direction="row"
+                      justifyContent="space-between"
+                    >
+                      <BlackInputLabel>From</BlackInputLabel>
+                      <BlackInputLabel>Balance: 56078</BlackInputLabel>
                     </Grid>
-                </Box>
-            </Paper>
-            {
-                !selectedWithdraw && (
-                    <div className={classes.setting} onClick={handleClickRelayerSetting}>
-                        <img src={settingImg}></img>
-                    </div>
-                )
-            }
-        </div>
-    );
+                    <Grid
+                      item
+                      container
+                      direction="row"
+                      justifyContent="space-between"
+                      alignItems="center"
+                      sx={{ paddingTop: "10px" }}
+                    >
+                      <InputLabel sx={{ fontSize: "20px" }}>0.0</InputLabel>
+                      <FlexDiv>
+                        <div>MAX</div>
+                        <select
+                          labelId="network-select-label"
+                          value={coinType}
+                          onChange={handleCoinType}
+                          className="select"
+                        >
+                          <option value={0}>USDT YP</option>
+                          <option value={1}>BNB</option>
+                        </select>
+                      </FlexDiv>
+                    </Grid>
+                  </BalanceGrid>
+                  <Grid item sx={{ textAlign: "center", padding: "20px" }}>
+                    <img src={arrow_down} />
+                  </Grid>
+                  <BalanceGrid item>
+                    <Grid
+                      item
+                      container
+                      direction="row"
+                      justifyContent="space-between"
+                      sx={{ paddingBottom: "10px" }}
+                    >
+                      <BlackInputLabel>To</BlackInputLabel>
+                      <BlackInputLabel>Balance: 0.000000</BlackInputLabel>
+                    </Grid>
+                    <Grid
+                      item
+                      container
+                      direction="row"
+                      justifyContent="space-between"
+                    >
+                      <InputLabel sx={{ fontSize: "20px" }}>0.0</InputLabel>
+                      <InputLabel sx={{ fontSize: "20px" }}>USDT</InputLabel>
+                    </Grid>
+                  </BalanceGrid>
+                  <Grid item container>
+                    <CalculateButton
+                      variant="contained"
+                      color="secondary"
+                      fullWidth
+                      onClick={handleWithrawYield}
+                    >
+                      Withdraw Yield
+                    </CalculateButton>
+                  </Grid>
+                </Grid>
+              </>
+            ) : (
+              <>
+                <Grid item direction="column" justifyContent="space-between">
+                  <Grid item>
+                    <InputLabel>Sacred Claim</InputLabel>
+                    <CustomTextField
+                      multiline
+                      variant="outlined"
+                      size="small"
+                      InputProps={{ disableUnderline: true }}
+                      fullWidth
+                    />
+                  </Grid>
+                  <Grid item>
+                    <CalculateButton
+                      variant="contained"
+                      color="secondary"
+                      fullWidth
+                      sx={{ marginTop: "280px" }}
+                      onClick={handleCalculatePoints}
+                    >
+                      Calculate Points
+                    </CalculateButton>
+                  </Grid>
+                </Grid>
+              </>
+            )}
+          </Grid>
+        </Box>
+      </Paper>
+      {!selectedWithdraw && (
+        <SettingDiv onClick={handleClickRelayerSetting}>
+          <img src={settingImg}></img>
+        </SettingDiv>
+      )}
+    </div>
+  );
 };
 
 export default YieldManage;
