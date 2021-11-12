d11624b7e3d9a031529035a5e9c567a80cb652be
unipine <unipine29@gmail.com>
2021-11-12 13:58:15 -0600

    fix the UI of WaitingModal

--- src/components/WaitingModal.js
+++ src/components/WaitingModal.js
@@ -1,74 +1,65 @@
-import React from 'react';
-import { Grid, Typography, Box } from '@mui/material';
-import makeStyles from '@mui/styles/makeStyles';
+import React from "react";
+import { Grid, Typography, Box } from "@mui/material";
 import video from "../images/sacred_animated_white";
+import { styled } from "@mui/material/styles";
 
-const useStyles = makeStyles({
-    root: {
-        position: 'fixed',
-        zIndex: 1300,
-        inset: '0px',
-    },
-    body: {
-        zIndex: -1,
-        position: 'fixed',
-        inset: '0px',
-        backgroundColor: 'rgba(20, 20, 20, 0.85)',
-    },
-    style: {
-        position: 'absolute',
-        textAlign: 'center',
-        top: '50%',
-        left: '50%',
-        transform: 'translate(-50%, -50%)',
-        color: 'white',
-        bgcolor: 'transparent',
-        width: '100%',
-        border: 'none',
-    },
-    video: {
-        width: '250px',
-        height: '250px',
-        borderRadius: '100%',
-    },
-    content: {
-        paddingTop: '1.25rem',
-        color: 'white',
-        fontSize: 18,
-        fontFamily: [
-            'Montserrat'
-        ]
-    }
-})
+const FixedBox = styled(Box)`
+  backgroundcolor: rgba(20, 20, 20, 0.85);
+  position: fixed;
+  zindex: 1300;
+  inset: 0;
+`;
+
+const ContentBox = styled(Box)`
+  position: absolute;
+  text-align: center;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  color: white;
+  bgcolor: transparent;
+  width: 100%;
+  border: none;
+`;
+
+const VideoContent = styled("img")`
+  width: 250px;
+  height: 250px;
+  border-radius: 100%;
+`;
+
+const TextContent = styled(Typography)`
+  padding-top: 1.25rem;
+  color: white;
+  font-size: 18px;
+  font-family: Montserrat;
+`;
 
 const WaitingModal = (prop) => {
-    const classes = useStyles();
 
-    return (
-        <Box className={classes.root}>
-            <Box className={classes.body} />
-            <Box className={classes.style}>
-                <Grid item xs={12}>
-                    <Grid
-                        item
-                        xs
-                        container
-                        spacing={2}
-                        direction="row"
-                        justifyContent="center"
-                        alignItems="center"
-                    >
-                        <Grid item>
-                            <img src={video} className={classes.video}></img>
-                        </Grid>
-                    </Grid>
-                </Grid>
-                <Typography className={classes.content}>
-                    {prop.content}
-                </Typography>
-            </Box>
-        </Box>
-    );
-}
+  return (
+    <FixedBox>
+      <Box sx={{ zIndex: -1 }} />
+      <ContentBox>
+        <Grid item xs={12}>
+          <Grid
+            item
+            xs
+            container
+            spacing={2}
+            direction="row"
+            justifyContent="center"
+            alignItems="center"
+          >
+            <Grid item>
+              <VideoContent src={video}></VideoContent>
+            </Grid>
+          </Grid>
+        </Grid>
+        <TextContent>{prop.content}</TextContent>
+      </ContentBox>
+    </FixedBox>
+  );
+};
 
 export default WaitingModal;
