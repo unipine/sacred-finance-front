bbac0e54d2ccdc3a29bb3c975697086f69a02c11
unipine <unipine29@gmail.com>
2021-11-15 12:08:47 -0600

    remove 3rd party location query for IP

--- src/components/FooterLeft.js
+++ src/components/FooterLeft.js
@@ -1,5 +1,5 @@
 import Grid from '@mui/material/Grid';
-// import getIPs from '../utils/webrc-ip';
+import getIPs from '../utils/webrc-ip';
 import { useState, useEffect } from 'react';
 import { useTranslation } from "react-i18next";
 
@@ -10,27 +10,27 @@ const FooterLeft = () => {
   const [ip, setIp] = useState();
   const [location, setLocation] = useState()
 
-  // useEffect(() => {
+  useEffect(() => {
 
-  //   // Gets IP address WITHOUT 3rd party but missing location details
-  //   // getIPs().then(data => {
-  //   //   setIp(data);
-  //   // })
+    // Gets IP address WITHOUT 3rd party but missing location details
+    getIPs().then(data => {
+      setIp(data);
+    })
 
-  //   // This call sends IP address to a 3rd party (geoip) to get location
-  //   fetch('https://ip.tornado.cash')
-  //     .then(res => res.json())
-  //     .then((result) => {
-  //       setIp(result.ip);
-  //       setLocation(`${result.city}, ${result.region}, ${result.country}`);
-  //     })
-  // }, [])
+    // This call sends IP address to a 3rd party (geoip) to get location
+    // fetch('https://ip.tornado.cash')
+    //   .then(res => res.json())
+    //   .then((result) => {
+    //     setIp(result.ip);
+    //     setLocation(`${result.city}, ${result.region}, ${result.country}`);
+    //   })
+  }, [])
 
   return (
 
     <Grid item xs={3}>
       <p className='ip-display'>
-        <b>{t("Your IP: ")}</b> {ip}, {location}
+        <b>{t("Your IP: ")}</b> {ip}
       </p>
 
     </Grid>
