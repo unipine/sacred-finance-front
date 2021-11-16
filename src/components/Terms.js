a5b6da9f2ef2a25ec154581308096d2935007cd2
unipine <unipine29@gmail.com>
2021-11-15 21:26:03 -0600

    update the language text

--- src/components/Terms.js
+++ src/components/Terms.js
@@ -1,6 +1,9 @@
 import React from 'react'
+import { useTranslation } from "react-i18next";
 
 const Terms = () => {
+  const { t } = useTranslation();
+  
   return (
     <div>
       <p><strong>{t('terms.text1')}</strong></p>
@@ -89,43 +92,43 @@ const Terms = () => {
 
       <p>{t('terms.text68')}“<strong>{t('terms.text69')}</strong>”), {t('terms.text70')}</p>
 
-      <p><strong>{t('terms.text')}</strong></p>
+      <p><strong>{t('terms.text71')}</strong></p>
 
-      <p>{t('terms.text')}The Site and the Services are provided on an “as is” and “as available” basis. Use of the Site and the Services is at your own risk. To the maximum extent permitted by applicable law, the Site and the Services are provided without warranties of any kind, whether express or implied, including, but not limited to, implied warranties of merchantability, fitness for a particular purpose, or non-infringement. No advice or information, whether oral or written, obtained by you from a Sacred Party or through the Site or the Services will create any warranty not expressly stated herein. Without limiting the foregoing, the Sacred Parties do not warrant that the content is accurate, reliable or correct; that the Site and the Services will meet your requirements; that the Site and the Services will be available at any particular time or location, uninterrupted or secure; that any defects or errors will be corrected; or that the Site and the Services are free of viruses or other harmful components. Any content downloaded or otherwise obtained through the use of the Site or the Services is downloaded at your own risk and you will be solely responsible for any damage to your computer system or mobile device or loss of data that results from such download or your use of the Site or the Services.</p>
+      <p>{t('terms.text72')}</p>
 
-      <p>{t('terms.text')}We do not warrant, endorse, guarantee, or assume responsibility for any product or site advertised or offered by a third party through the Site or the Services or any hyperlinked website or site, and we will not be a party to or in any way monitor any transaction between you and third-party providers of products or services.</p>
+      <p>{t('terms.text73')}</p>
 
-      <p><strong>{t('terms.text')}11. Disclaimers</strong></p>
+      <p><strong>{t('terms.text74')}</strong></p>
 
-      <p>{t('terms.text')}None of the Sacred Parties will have any responsibilities or liability with respect to the following: (a) the Services could be impacted by one or more regulatory inquiries or actions, which could prevent or limit our ability to continue to develop or provide the Services, or for you and your users to use the Services, (b) we have no obligation to update the Services or its underlying platforms and networks to address, mitigate, or remediate any security or other vulnerabilities in the Services, or such platforms or networks, and (c) portions of the Services or any other underlying networks and platforms may rest on open-source software, and there is a risk that weaknesses or bugs that may be introduced in the infrastructural elements of the Services or any other underlying networks and platforms, which may result in security vulnerabilities, data loss, damage, destructions, disclosure, or other compromises.</p>
+      <p>{t('terms.text75')}</p>
 
-      <p><strong>{t('terms.text')}12. Assumption of Risk; Limitation of Liability</strong></p>
+      <p><strong>{t('terms.text76')}</strong></p>
 
-      <p>{t('terms.text')}You acknowledge that the Site and the Services, and your use of the Site and the Services contain certain risks, including without limitation the following risks: <strong>{t('terms.text')}That any Services you interact with are entirely your own responsibility and liability, and we are not a party to Protocol; at any time, your access to your digital assets may be suspended or terminated or there may be a delay in your access or use of your digital assets which may result in the digital assets diminishing in value; and the Services may be suspended or terminated for any or no reason, which may limit your access to your digital assets. </strong>{t('terms.text')}Accordingly, you expressly agree that you assume all risk in connection with your access and use of the Site and Services.</p>
+      <p>{t('terms.text77')}<strong>{t('terms.text78')}</strong>{t('terms.text79')}</p>
 
-      <p>{t('terms.text')}You also agree that to the maximum extent permitted by Applicable Laws, in no event shall any Sacred Party be liable for any indirect, punitive, incidental, special, consequential or exemplary damages, including without limitation damages for loss of profits, goodwill, use, data or other intangible losses, arising out of or relating to the use of, or inability to use, this Site or the Services. Under no circumstances will any Sacred Party be responsible for any damage, loss or injury resulting from hacking, tampering or other unauthorized access or use of the Site, the Services or the information contained therein. To the maximum extent permitted by Applicable Laws, we assume no liability or responsibility for any (a) errors, mistakes, or inaccuracies of content; (b) personal injury or property damage, of any nature whatsoever, resulting from your access to or use of our Site or the Services; (c) any unauthorized access to or use of our secure servers and/or any and all personal information stored therein; (d) any interruption or cessation of transmission to or from the Site or the Services; (e) any bugs, viruses, trojan horses, or the like that may be transmitted to or through our Site or the Services by any third party; (f) any errors or omissions in any content or for any loss or damage incurred as a result of the use of any content posted, emailed, transmitted, or otherwise made available through the Site or the Services; and/or (g) User Content or the defamatory, offensive, or illegal conduct of any third party. You agree that if, notwithstanding the other provisions of this Agreement, a Sacred Party is found to be liable for any claims, proceedings, liabilities, obligations, damages, losses or costs, such Sacred Party’s liability shall in no event exceed the amount you paid to us hereunder.</p>
+      <p>{t('terms.text80')}</p>
 
-      <p>{t('terms.text')}This limitation of liability section applies whether the alleged liability is based on contract, tort, negligence, strict liability, or any other basis, even if we have been advised of the possibility of such damage. The foregoing limitation of liability shall apply to the fullest extent permitted by law in the applicable jurisdiction.</p>
+      <p>{t('terms.text81')}</p>
 
-      <p><strong>{t('terms.text')}13. Limitations as Allowed by Law</strong></p>
+      <p><strong>{t('terms.text82')}</strong></p>
 
-      <p>{t('terms.text')}Some states, provinces and other jurisdictions do not allow the exclusion and limitations of certain implied warranties, or the exclusion or limitation of incidental or consequential damages, so the above limitations or exclusions may not apply to you. This Agreement gives you specific legal rights, and you may also have other rights which vary from jurisdiction to jurisdiction. The disclaimers, exclusions, and limitations of liability under this agreement will not apply to the extent prohibited by applicable law.</p>
+      <p>{t('terms.text83')}</p>
 
-      <p><strong>{t('terms.text')}14. Non-Waiver</strong></p>
+      <p><strong>{t('terms.text84')}</strong></p>
 
-      <p>Our failure or delay in insisting upon or enforcing strict performance of any provision of this Agreement shall not be construed as a waiver of any provision or rights. No waiver of any provision of this Agreement shall constitute a waiver of any other provision, nor shall any waiver of any provision of this Agreement constitute a continuing waiver unless expressly made in writing by us.</p>
+      <p>{t('terms.text85')}</p>
 
-      <p><strong>{t('terms.text')}15. Severability</strong></p>
+      <p><strong>{t('terms.text86')}</strong></p>
 
-      <p>{t('terms.text')}In the event that any of the provisions of this Agreement are held by a court or other tribunal of competent jurisdiction to be unenforceable, such provisions shall be limited or eliminated to the minimum extent necessary so that these terms and conditions shall otherwise remain in full force and effect.</p>
+      <p>{t('terms.text87')}</p>
 
-      <p><strong>{t('terms.text')}16. Governing Law and Jurisdiction</strong></p>
+      <p><strong>{t('terms.text88')}</strong></p>
 
-      <p>{t('terms.text')}This Agreement is entered into in Singapore and shall be governed by, and construed in accordance with, the laws of Singapore. You agree to submit to the exclusive jurisdiction of the courts of Singapore or any other judicial district or jurisdiction as we may determine in any and all actions, disputes or controversies relating hereto. You further agree as follows: (a) any claim brought to enforce these terms and conditions must be commenced within two (2) years of the cause of action accruing; (b) no recovery may be sought or received for damages other than out-of-pocket expenses, except that the prevailing party will be entitled to costs and legal fees; and (c) any claim must be brought individually and not consolidated as part of a group or class action complaint.</p>
+      <p>{t('terms.text89')}</p>
 
-      <p><strong>{t('terms.text')}17. Language</strong></p>
+      <p><strong>{t('terms.text90')}</strong></p>
 
-      <p>{t('terms.text')}This Agreement, including all other documents incorporated by reference herein, are binding and constitute the entire agreement between us and you with respect to your use of the Site and the Services. The parties agree that the English language will be the language of the Agreement and all documents in connection thereto, and each party waives any right (whether statutory or otherwise) to use and rely upon any other language, or translations. <em> Il est de la volonté expresse des parties que tous les documents qui s'y rattachent soient rédigés en langue anglaise.</em></p>
+      <p>{t('terms.text91')}<em>{t('terms.text92')}</em></p>
 
 
     </div>
