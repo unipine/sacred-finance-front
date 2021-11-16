26386bdd206a35993435881c31ff92caa191ae95
unipine <unipine29@gmail.com>
2021-11-15 21:28:16 -0600

    add the lanaugage text for term component

--- src/components/Terms.js
+++ src/components/Terms.js
@@ -1,138 +1,214 @@
-import React from 'react'
+import React from "react";
 import { useTranslation } from "react-i18next";
 
 const Terms = () => {
   const { t } = useTranslation();
-  
+
   return (
     <div>
-      <p><strong>{t('terms.text1')}</strong></p>
+      <p>
+        <strong>{t("terms.text1")}</strong>
+      </p>
 
-      <p>{t('terms.text2')}</p>
+      <p>{t("terms.text2")}</p>
 
-      <p>{t('terms.text3')} (“<strong>{t('terms.text4')}</strong>”, “<strong>{t('terms.text5')}</strong>” {t('terms.text6')} “<strong>{t('terms.text7')}</strong>”). {t('terms.text8')} “<strong>{t('terms.text9')}</strong>”) {t('terms.text10')} “<strong>{t('terms.text11')}</strong>”), {t('terms.text12')} (“<strong>{t('terms.text16')}</strong>” {t('terms.text13')} “<strong>{t('terms.text14')}</strong>”).</p>
+      <p>
+        {t("terms.text3")} (“<strong>{t("terms.text4")}</strong>”, “
+        <strong>{t("terms.text5")}</strong>” {t("terms.text6")} “
+        <strong>{t("terms.text7")}</strong>”). {t("terms.text8")} “
+        <strong>{t("terms.text9")}</strong>”) {t("terms.text10")} “
+        <strong>{t("terms.text11")}</strong>”), {t("terms.text12")} (“
+        <strong>{t("terms.text16")}</strong>” {t("terms.text13")} “
+        <strong>{t("terms.text14")}</strong>”).
+      </p>
 
-      <p>{t('terms.text15')}</p>
+      <p>{t("terms.text15")}</p>
 
-      <p><strong>{t('terms.text17')}</strong></p>
+      <p>
+        <strong>{t("terms.text17")}</strong>
+      </p>
 
-      <p>{t('terms.text18')} “<strong>{t('terms.text19')}</strong>”) {t('terms.text20')} “<strong>{t('terms.text21')}</strong>”).</p>
+      <p>
+        {t("terms.text18")} “<strong>{t("terms.text19")}</strong>”){" "}
+        {t("terms.text20")} “<strong>{t("terms.text21")}</strong>”).
+      </p>
 
-      <p><strong>{t('terms.text22')}</strong></p>
+      <p>
+        <strong>{t("terms.text22")}</strong>
+      </p>
 
-      <p><strong>{t('terms.text23')}</strong></p>
+      <p>
+        <strong>{t("terms.text23")}</strong>
+      </p>
 
-      <p>{t('terms.text24')}(“<strong>{t('terms.text25')}</strong>”); {t('terms.text26')}“<strong>{t('terms.text27')}</strong>”). {t('terms.text28')}</p>
+      <p>
+        {t("terms.text24")}(“<strong>{t("terms.text25")}</strong>”);{" "}
+        {t("terms.text26")}“<strong>{t("terms.text27")}</strong>”).{" "}
+        {t("terms.text28")}
+      </p>
 
-      <p>{t('terms.text29')}</p>
+      <p>{t("terms.text29")}</p>
 
-      <p>{t('terms.text30')}</p>
+      <p>{t("terms.text30")}</p>
 
-      <p>{t('terms.text31')}</p>
+      <p>{t("terms.text31")}</p>
 
-      <p>{t('terms.text32')}</p>
+      <p>{t("terms.text32")}</p>
 
-      <p>{t('terms.text33')}</p>
+      <p>{t("terms.text33")}</p>
 
-      <p>{t('terms.text34')}</p>
+      <p>{t("terms.text34")}</p>
 
-      <p><strong>{t('terms.text35')}</strong></p>
+      <p>
+        <strong>{t("terms.text35")}</strong>
+      </p>
 
-      <p>{t('terms.text36')}</p>
+      <p>{t("terms.text36")}</p>
 
-      <p><strong>{t('terms.text37')}</strong></p>
+      <p>
+        <strong>{t("terms.text37")}</strong>
+      </p>
 
-      <p>{t('terms.text38')}</p>
+      <p>{t("terms.text38")}</p>
 
-      <p>{t('terms.text39')}</p>
+      <p>{t("terms.text39")}</p>
 
-      <p>{t('terms.text40')}</p>
+      <p>{t("terms.text40")}</p>
 
-      <p><strong>{t('terms.text41')}</strong></p>
+      <p>
+        <strong>{t("terms.text41")}</strong>
+      </p>
 
-      <p>{t('terms.text42')}</p>
+      <p>{t("terms.text42")}</p>
 
-      <p>{t('terms.text43')}</p>
+      <p>{t("terms.text43")}</p>
 
-      <p>{t('terms.text44')}</p>
+      <p>{t("terms.text44")}</p>
 
-      <p><strong>{t('terms.text45')}</strong></p>
+      <p>
+        <strong>{t("terms.text45")}</strong>
+      </p>
 
-      <p>{t('terms.text46')}</p>
+      <p>{t("terms.text46")}</p>
 
-      <p><strong>{t('terms.text47')}</strong></p>
+      <p>
+        <strong>{t("terms.text47")}</strong>
+      </p>
 
-      <p>{t('terms.text48')}(“<strong>{t('terms.text49')}</strong>”){t('terms.text50')} “<strong>{t('terms.text51')}</strong>”), {t('terms.text52')}</p>
+      <p>
+        {t("terms.text48")}(“<strong>{t("terms.text49")}</strong>”)
+        {t("terms.text50")} “<strong>{t("terms.text51")}</strong>”),{" "}
+        {t("terms.text52")}
+      </p>
 
-      <p>{t('terms.text53')}(“<strong>{t('terms.text55')}</strong>”). {t('terms.text54')}</p>
+      <p>
+        {t("terms.text53")}(“<strong>{t("terms.text55")}</strong>”).{" "}
+        {t("terms.text54")}
+      </p>
 
-      <p><strong>{t('terms.text56')}</strong></p>
+      <p>
+        <strong>{t("terms.text56")}</strong>
+      </p>
 
-      <p>{t('terms.text57')}</p>
+      <p>{t("terms.text57")}</p>
 
-      <p><strong>{t('terms.text58')}</strong></p>
+      <p>
+        <strong>{t("terms.text58")}</strong>
+      </p>
 
-      <p>{t('terms.text59')}</p>
+      <p>{t("terms.text59")}</p>
 
-      <p><strong>{t('terms.text60')}</strong></p>
+      <p>
+        <strong>{t("terms.text60")}</strong>
+      </p>
 
-      <p>{t('terms.text61')}</p>
+      <p>{t("terms.text61")}</p>
 
-      <p><strong>{t('terms.text62')}</strong></p>
+      <p>
+        <strong>{t("terms.text62")}</strong>
+      </p>
 
-      <p>{t('terms.text63')}</p>
+      <p>{t("terms.text63")}</p>
 
-      <p>{t('terms.text64')}</p>
+      <p>{t("terms.text64")}</p>
 
-      <p><strong>{t('terms.text65')}</strong></p>
+      <p>
+        <strong>{t("terms.text65")}</strong>
+      </p>
 
-      <p>{t('terms.text66')}</p>
+      <p>{t("terms.text66")}</p>
 
-      <p><strong>{t('terms.text67')}</strong></p>
+      <p>
+        <strong>{t("terms.text67")}</strong>
+      </p>
 
-      <p>{t('terms.text68')}“<strong>{t('terms.text69')}</strong>”), {t('terms.text70')}</p>
+      <p>
+        {t("terms.text68")}“<strong>{t("terms.text69")}</strong>”),{" "}
+        {t("terms.text70")}
+      </p>
 
-      <p><strong>{t('terms.text71')}</strong></p>
+      <p>
+        <strong>{t("terms.text71")}</strong>
+      </p>
 
-      <p>{t('terms.text72')}</p>
+      <p>{t("terms.text72")}</p>
 
-      <p>{t('terms.text73')}</p>
+      <p>{t("terms.text73")}</p>
 
-      <p><strong>{t('terms.text74')}</strong></p>
+      <p>
+        <strong>{t("terms.text74")}</strong>
+      </p>
 
-      <p>{t('terms.text75')}</p>
+      <p>{t("terms.text75")}</p>
 
-      <p><strong>{t('terms.text76')}</strong></p>
+      <p>
+        <strong>{t("terms.text76")}</strong>
+      </p>
 
-      <p>{t('terms.text77')}<strong>{t('terms.text78')}</strong>{t('terms.text79')}</p>
+      <p>
+        {t("terms.text77")}
+        <strong>{t("terms.text78")}</strong>
+        {t("terms.text79")}
+      </p>
 
-      <p>{t('terms.text80')}</p>
+      <p>{t("terms.text80")}</p>
 
-      <p>{t('terms.text81')}</p>
+      <p>{t("terms.text81")}</p>
 
-      <p><strong>{t('terms.text82')}</strong></p>
+      <p>
+        <strong>{t("terms.text82")}</strong>
+      </p>
 
-      <p>{t('terms.text83')}</p>
+      <p>{t("terms.text83")}</p>
 
-      <p><strong>{t('terms.text84')}</strong></p>
+      <p>
+        <strong>{t("terms.text84")}</strong>
+      </p>
 
-      <p>{t('terms.text85')}</p>
+      <p>{t("terms.text85")}</p>
 
-      <p><strong>{t('terms.text86')}</strong></p>
+      <p>
+        <strong>{t("terms.text86")}</strong>
+      </p>
 
-      <p>{t('terms.text87')}</p>
+      <p>{t("terms.text87")}</p>
 
-      <p><strong>{t('terms.text88')}</strong></p>
+      <p>
+        <strong>{t("terms.text88")}</strong>
+      </p>
 
-      <p>{t('terms.text89')}</p>
+      <p>{t("terms.text89")}</p>
 
-      <p><strong>{t('terms.text90')}</strong></p>
+      <p>
+        <strong>{t("terms.text90")}</strong>
+      </p>
 
-      <p>{t('terms.text91')}<em>{t('terms.text92')}</em></p>
-
-
+      <p>
+        {t("terms.text91")}
+        <em>{t("terms.text92")}</em>
+      </p>
     </div>
-  )
-}
+  );
+};
 
-export default Terms
+export default Terms;
